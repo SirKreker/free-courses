194f5416382aa957fd496c8e6da3d00617137e25
SirKreker <il.egorov-2011@yandex.ru>
2018-06-03 17:36:47 +0300

    complete task 2

--- task2.js
+++ task2.js
@@ -5,9 +5,46 @@ var z1 = process.argv[2];
 var z2 = process.argv[3];
 var l1 = 0;
 var l2 = 0;
+var str1 = '';
+var str2 = '';
 l1 = z1.length-1;
 l2 = z2.length-1;
 if (l1!=l2){
-    
+    console.log('-1');
+} else {
+    var s = 10001;
+    for (var i=1; i<l1; i++){
+        str1 +=z1[i];
+        str2 +=z2[i];
+    }
+    var l3 = 0;
+    l3 = str1.length;
+    for (i=0; i<l3; i++){
+        if(str1[0]==str2[i]){
+            var k = 0;
+            for (var j=0; j<l3; j++){
+                m = j + i;
+                if (m>l3-1){
+                    m -= l3; 
+                }
+                if (str1[j]!=str2[m]){
+                    k++;
+                }
+            }
+            if (k == 0){
+                if (i<s){
+                    s = i;
+                }
+                var f = l3 - i;
+                if (f<s){
+                    s = f;
+                }
+            }
+        }
+    }
+    if (s<10001){
+        console.log(s);
+    } else {
+        console.log('-1');
+    }
 }
-process.stdout.write(s);
