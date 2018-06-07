01e82089350bdbe483f3818cfd9972ed792b59ac
SirKreker <il.egorov-2011@yandex.ru>
2018-06-07 14:29:52 +0300

    regEXP

--- task1.js
+++ task1.js
@@ -2,10 +2,5 @@
 
 /* Put your code here */
 var str = process.argv[2];
-var n = 0;
-n = str.length-1;
-for (var i=1; i<n; i++){
-    if ((str[i]!='Р')&&(str[i]!='К')&&(str[i]!='Н')&&(str[i]!='р')&&(str[i]!='к')&&(str[i]!='н')){
-        process.stdout.write(str[i]);
-    }
-}
+str = str.replace(/[ркн]/gi,'');
+process.stdout.write(str);
