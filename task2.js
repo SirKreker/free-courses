add54d674279b3af202b7d681785434c554caac4
SirKreker <il.egorov-2011@yandex.ru>
2018-06-08 21:12:44 +0300

    rework

--- task2.js
+++ task2.js
@@ -1,31 +1,23 @@
 /* Task 2 */
 
 /* Put your code here */
-var z1 = process.argv[2];
-var z2 = process.argv[3];
-var l1 = 0;
-var l2 = 0;
-var str1 = '';
-var str2 = '';
-l1 = z1.length-1;
-l2 = z2.length-1;
+var str1 = process.argv[2];
+var str2 = process.argv[3];
+str1 = ''+str1;
+str2 = ''+str2;
+var l1 = str1.length;
+var l2 = str2.length;
 if (l1!=l2){
     process.stdout.write('-1');
 } else {
     var s = 10001;
-    for (var i=1; i<l1; i++){
-        str1 +=z1[i];
-        str2 +=z2[i];
-    }
-    var l3 = 0;
-    l3 = str1.length;
-    for (i=0; i<l3; i++){
+    for (i=0; i<l1; i++){
         if(str1[0]==str2[i]){
             var k = 0;
-            for (var j=0; j<l3; j++){
+            for (var j=0; j<l1; j++){
                 m = j + i;
-                if (m>l3-1){
-                    m -= l3; 
+                if (m>l1-1){
+                    m -= l1; 
                 }
                 if (str1[j]!=str2[m]){
                     k++;
@@ -35,7 +27,7 @@ if (l1!=l2){
                 if (i<s){
                     s = i;
                 }
-                var f = l3 - i;
+                var f = l1 - i;
                 if (f<s){
                     s = f;
                 }
